2b8d8f14bde8001eaaa2952f04b1193ca059cda9
Hou <5466315@qq.com>
2016-06-21 09:05:18 +0800

    添加第一次加载页面执行路由

--- router.js
+++ router.js
@@ -2,7 +2,7 @@
     function Router(){
         var self = this;
         root.onhashchange = function () {
-            self.do();
+            self.do(root.location.hash);
         };
         self.router = [];
     };
@@ -27,11 +27,9 @@
         }
         return this;
     };
-    Router.prototype.do = function(){
+    Router.prototype.do = function(hash){
         var isHaveRouter = false;
         var isHaveDefaultRouter = false;
-
-        var hash = root.location.hash;
         var hashArr = hash.split('/');
         for(var i=0,length = this.router.length; i<length; i++){
             if(hash.match(this.router[i].reg)){
@@ -55,6 +53,10 @@
     };
     Router.prototype.default = function(name){
         this.defaultRouter = name;
+        return this;
+    };
+    Router.prototype.complete = function(){
+        this.do(root.location.hash);
     };
     return root.$router = Router;
 })(window);
